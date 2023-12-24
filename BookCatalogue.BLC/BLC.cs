67b0e0c67e73fc3ab474fef5066324bc9882f81b
AlekOwcz <96996586+AlekOwcz@users.noreply.github.com>
2023-12-24 03:12:00 +0100

    Finish DAOMock tutorial

---
+++ BookCatalogue.BLC/BLC.cs
@@ -0,0 +1,40 @@
+﻿using BookCatalogue.Interfaces;
+using System.Reflection;
+
+namespace BookCatalogue.BLC
+{
+    public class BLC
+    {
+        private IDAO dao;
+        //statyczna metoda get instance, sprawdzic czy obiekt pusty
+        public BLC(string libraryName)
+        {
+            Type? typeToCreate = null;
+            //todo try catch 
+            Assembly assembly = Assembly.UnsafeLoadFrom(libraryName);
+
+            foreach (var type in assembly.GetTypes())
+            {
+                if (type.IsAssignableTo(typeof(IDAO))) 
+                {
+                    typeToCreate = type;
+                    break;
+                }
+            }
+
+            //todo null check
+
+            dao = (IDAO)Activator.CreateInstance(typeToCreate, null);
+        }
+
+        public IEnumerable<IAuthor> GetAuthors()
+        {
+            return dao.GetAllAuthors();
+        }
+
+        public IEnumerable<IBook> GetBooks()
+        {
+            return dao.GetAllBooks();
+        }
+    }
+}
--- BookCatalogue/Program.cs
+++ BookCatalogue/Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            BLC.BLC blc = new BLC.BLC("BookCatalogue.DAOMock.dll");
+
+            foreach (var a in blc.GetAuthors()) 
+            {
+                Console.WriteLine($"{a.ID}: {a.Name} {a.Surname}");
+            }
+
+            foreach (var b in blc.GetBooks())
+            {
+                Console.WriteLine($"{b.ID}: {b.Author.Name} {b.Author.Surname}: {b.Title}, {b.ReleaseYear}");
+            }
         }
     }
 }
